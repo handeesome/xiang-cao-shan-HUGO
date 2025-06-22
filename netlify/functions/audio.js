20fe9a7cdf6b25aaa6e1e0fcf046b39064834147
cenhan <ducenhandee@gmail.com>
2025-06-22 20:58:13 +0800

    update audio url

--- netlify/functions/audio.js
+++ netlify/functions/audio.js
@@ -7,6 +7,7 @@ const s3 = new S3({
   region: process.env.OSS_REGION,
   signatureVersion: "v4",
 });
+
 const bucketName = process.env.OSS_BUCKET;
 
 exports.handler = async (event) => {
@@ -19,11 +20,25 @@ exports.handler = async (event) => {
     };
   }
 
+  // Extract path name from the request path
+  const path = event.path;
+  const match = path.match(/\/([^/]+)$/);
+  const folder = match ? decodeURIComponent(match[1]) : "";
+
+  if (!folder) {
+    return {
+      statusCode: 400,
+      body: "Missing folder name in function path",
+    };
+  }
+
+  const objectKey = `${folder}/${file}`;
+
   try {
     const signedUrl = s3.getSignedUrl("getObject", {
       Bucket: bucketName,
-      Key: `${file}`,
-      Expires: 60, // 链接有效期：单位秒
+      Key: objectKey,
+      Expires: 60,
     });
 
     return {
